5195c34b22064a71be951436badb14ff8030f0dc
Orrin Manning <omanning@vivafirst.com>
2022-09-06 23:20:37 -0500

    Wrap lines to max of 80 characters

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,8 @@ void main() {
 /// We are using a StatefulWidget such that we only create the [Future] once,
 /// no matter how many times our widget rebuild.
 /// If we used a [StatelessWidget], in the event where [App] is rebuilt, that
-/// would re-initialize FlutterFire and make our application re-enter loading state,
-/// which is undesired.
+/// would re-initialize FlutterFire and make our application re-enter loading
+/// state, which is undesired.
 class App extends StatefulWidget {
   const App({super.key});
 
@@ -22,8 +22,8 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  /// The future is part of the state of our widget. We should not call `initializeApp`
-  /// directly inside [build].
+  /// The future is part of the state of our widget. We should not call
+  /// `initializeApp` directly inside [build].
   final Future<FirebaseApp> _initialization =
       Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
@@ -46,7 +46,8 @@ class _AppState extends State<App> {
           );
         }
 
-        // Otherwise, show something whilst waiting for initialization to complete
+        // Otherwise, show something whilst waiting for initialization to
+        // complete
         return const Text('loading');
       },
     );
