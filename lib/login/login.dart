c9d86c9a58785168f8858508bc865f2d45498f0c
Orrin Manning <omanning@vivafirst.com>
2022-09-08 00:27:54 -0500

    Add sign in with Apple button

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:quizapp/services/auth.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -24,6 +25,18 @@ class LoginScreen extends StatelessWidget {
                   color: Colors.blue,
                   loginMethod: AuthService().googleLogin,
                 ),
+              ),
+              Flexible(
+                child: SignInWithAppleButton(onPressed: () async {
+                  final credential = await SignInWithApple.getAppleIDCredential(
+                    scopes: [
+                      AppleIDAuthorizationScopes.email,
+                      AppleIDAuthorizationScopes.fullName,
+                    ],
+                  );
+
+                  print(credential);
+                }),
               ),
               Flexible(
                 child: LoginButton(
